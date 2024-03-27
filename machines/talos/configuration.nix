60455fd25f0a14d757f5c9b7c15bcead8ab3f326
Andrew Williams <andy@tensixtyone.com>
2024-03-27 16:01:49 +0000

    Cleanup config

--- machines/talos/configuration.nix
+++ machines/talos/configuration.nix
@@ -10,6 +10,9 @@
       ./hardware-configuration.nix
     ];
 
+  system.copySystemConfiguration = true;
+  system.stateVersion = "23.11";
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -20,8 +23,9 @@
     ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  networking.hostName = "talos"; # Define your hostname.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.hostName = "talos";
+  networking.networkmanager.enable = true;
+  networking.firewall.enable = true;
 
   # Set your time zone.
   time.timeZone = "Europe/London";
@@ -34,24 +38,12 @@
   };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = false;
+  services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
-  #services.xserver.displayManager.gdm.wayland = false;
+  services.xserver.displayManager.gdm.wayland = true;
   services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
   services.xserver.xkb.layout = "us";
-
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-
-  # Enable sensors
-  hardware.sensor.iio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
   services.xserver.libinput.enable = true;
-
   services.xserver.xrandrHeads =
   [
     {
@@ -62,6 +54,13 @@
       '';
     }
   ];
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable sensors
+  hardware.sensor.iio.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users = {
@@ -76,8 +75,7 @@
     };
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # List packages installed in system profile
   environment.systemPackages = with pkgs; [
     git
     lsof
@@ -106,29 +104,4 @@
     fwupd.enable = true;
     smartd.enable = true;
   };
-
-  networking.firewall.enable = true;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "23.11"; # Did you read the comment?
 }
