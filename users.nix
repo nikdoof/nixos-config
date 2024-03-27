95a6fb71e0958f629e0b58ed8d61ceabf2472165
Andrew Williams <andy@tensixtyone.com>
2024-03-27 22:04:34 +0000

    Add user config

--- users.nix
+++ users.nix
@@ -8,11 +8,18 @@
       extraGroups = 
       [ 
         "wheel"
+        "networkmanager"
         "video" 
+        "audio"
       ];
+      linger = true;
+      initialPassword = "pass1234";
       shell = pkgs.zsh;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHWO2qwHaPaQs46na4Aa6gMkw5QqRHUMGQphtgAcDJOw"
+      ];
+      packages = with pkgs; [
+        tmux
       ];
     };
   };
