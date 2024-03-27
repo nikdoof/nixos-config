fdbef8f73061987aae5df851e61456fddf21092b
Andrew Williams <andy@tensixtyone.com>
2024-03-27 22:13:08 +0000

    Add auto-cpufreq

--- machines/talos/configuration.nix
+++ machines/talos/configuration.nix
@@ -35,4 +35,16 @@
   };
 
   services.tailscale.enable = true;
+
+  services.auto-cpufreq.enable = true;
+  services.auto-cpufreq.settings = {
+    battery = {
+      governor = "powersave";
+      turbo = "never";
+    };
+    charger = {
+      governor = "performance";
+      turbo = "auto";
+    };
+  };
 }
